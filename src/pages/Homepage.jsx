0365b4d1a5bdcdda93efc7afd461b977b26ec591
YesitsmePop <morgandeanmcd@gmail.com>
2025-08-07 20:02:33 -0700

    Second fix for images

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-
+import sticker1 from "../assets/images/sticker1.jpg";
+import sticker2 from "../assets/images/sticker2.jpg";
+import sticker3 from "../assets/images/sticker3.jpg";
 import "./stickers.css"; 
 
 const stickers = [
-  { id: 1, src: "/images/sticker1.jpg", alt: "Sticker 1" },
-  { id: 2, src: "/images/sticker2.jpg", alt: "Sticker 2" },
-  { id: 3, src: "/images/sticker3.jpg", alt: "Sticker 3" },
+  { id: 1, src: sticker1, alt: "Sticker 1" },
+  { id: 2, src: sticker2, alt: "Sticker 2" },
+  { id: 3, src: sticker3, alt: "Sticker 3" },
 ];
 
 export default function Homepage() {
